1e6e0cee9a42eed34b1ab0cbbfe437d8efaf320b
ABis-here <augustasbaublys14@gmail.com>
2025-09-23 02:45:24 +0300

    Add third button, that changes background colour

--- app/src/main/java/com/example/manoappsas/MainActivity.java
+++ app/src/main/java/com/example/manoappsas/MainActivity.java
@@ -5,13 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.graphics.Color;
 
+import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
     @SuppressLint("SetTextI18n")
@@ -22,19 +24,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView tv = findViewById(R.id.tvHello);
-        Button btn = findViewById(R.id.btnChangeText);
 
-        btn.setOnClickListener(v -> tv.setText("Nesupratau ar github repository prisidejo, nes error kazkoks buvo"));
+        Button btn = findViewById(R.id.btnChangeText);
+        btn.setOnClickListener(v ->
+                tv.setText("Nesupratau ar github repository prisidejo, nes error kazkoks buvo")
+        );
 
         Button btnColor = findViewById(R.id.btnChangeColor);
         btnColor.setOnClickListener(v ->
                 tv.setTextColor(Color.RED)
         );
 
+        View root = findViewById(R.id.main);
+        Button btnBg = findViewById(R.id.btnBgColor);
+        btnBg.setOnClickListener(v ->
+                root.setBackgroundColor(
+                        ContextCompat.getColor(this, android.R.color.holo_blue_light)
+                )
+        );
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-
             return insets;
         });
     }
